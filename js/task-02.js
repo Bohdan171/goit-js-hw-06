e4f5d827afea0633235d5efb564cae013892e3bb
Bohdan Onyshko <bogdan_onishko@ukr.net>
2022-05-27 22:18:37 +0300

    Пока так

---
+++ js/task-02.js
@@ -0,0 +1,19 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+const ingredientsItemAdd = ingredients.map((ingredient) => {
+  const ingredientsItemRef = document.createElement("li");
+  ingredientsItemRef.textContent = ingredient;
+  ingredientsItemRef.classList.add("item");
+
+  return ingredientsItemRef;
+});
+
+const ingredientsListRef = document.querySelector("#ingredients");
+
+ingredientsListRef.append(...ingredientsItemAdd);
