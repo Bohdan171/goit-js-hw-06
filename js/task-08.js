c4b37d58e794553d35af8adf15ed88538cdee222
Bohdan Onyshko <bogdan_onishko@ukr.net>
2022-05-28 23:00:46 +0300

    9 задач

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit (event){
+    event.preventDefault();
+    const {
+        elements: {email, password }
+      } = event.currentTarget;
+      const emailEl = event.currentTarget.email.value;
+      const passwordEl = event.currentTarget.password.value;
+      console.log({emailEl, passwordEl});
+    if (email.value === "" || password.value === "") {
+        return alert("Заполните форму!");
+      }
+     
+   
+   const formData = new FormData(event.currentTarget);
+    
+   
+
+    event.currentTarget.reset();
+
+   }
